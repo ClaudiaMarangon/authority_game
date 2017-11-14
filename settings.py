38a1f2ede05f4ca6a024eba5de01e85b79b3aa41
ClaudiaMarangon <claudia.marangon@studbocconi.it>
2017-11-14 13:40:09 +0100

    app completed

--- settings.py
+++ settings.py
@@ -136,6 +136,8 @@ SESSION_CONFIGS = [
         'num_demo_participants': 2,
         'app_sequence': [
             'authority_game',
+            'questionnaire',
+            'circle_task',
         ],
         'use_browser_bots': False,
     },
