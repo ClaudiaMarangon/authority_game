0e1da11ada9611bd882ecb9637eb6835f0d34009
ClaudiaMarangon <claudia.marangon@studbocconi.it>
2018-01-26 20:31:27 +0100

    final payoff added

--- final_payoff/models.py
+++ final_payoff/models.py
@@ -18,6 +18,14 @@ class Constants(BaseConstants):
 
 
 class Subsession(BaseSubsession):
+
+    def creating_session(self):
+        for p in self.get_players():
+            p.payoff_practice = p.participant.vars['pay']
+            p.payoff_simple_task = p.participant.vars['pay_nooffer']
+            p.payoff_task = p.participant.vars['task_payoff']
+            p.payoff_money = p.participant.vars['money_pay']
+            p.payoff_circle = p.participant.vars['circlet_payoff']
     pass
 
 
@@ -26,9 +34,13 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
+    payoff_practice = models.FloatField()
+    payoff_simple_task = models.FloatField()
+    payoff_task = models.FloatField()
+    payoff_money = models.FloatField()
+    payoff_circle = models.FloatField()
+
     def set_payoff(self):
-        self.ct_payoff = self.participant.vars['circlet_payoff']
-        self.tg_payoff = self.participant.vars['task_payoff']
-        self.payoff = self.ct_payoff + self.tg_payoff
+        self.payoff = self.payoff_simple_task + self.payoff_task + self.payoff_money + self.payoff_circle
         self.participant.vars['final_payoff'] = self.payoff
     pass
