dc30440b1926b3ac26157f2a9ff679d130bcf58b
ClaudiaMarangon <claudia.marangon@studbocconi.it>
2018-03-01 09:04:34 +0100

    22 p

--- final_payoff/models.py
+++ final_payoff/models.py
@@ -21,7 +21,7 @@ class Constants(BaseConstants):
 
 class Subsession(BaseSubsession):
     def creating_session(self):
-        n_player = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
+        n_player = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]
         for p in self.get_players():
             p.lucky = random.choice(n_player)
             n_player.remove(p.lucky)
